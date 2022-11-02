79cf04862da533ee32a2dc4853615582cb82048c
EngYahye <yahye20025@gmail.com>
2022-11-02 13:23:50 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
--- frontend/src/Screens/Homescreen.js
+++ frontend/src/Screens/Homescreen.js
@@ -1,82 +1,79 @@
-import { Link } from "react-router-dom";
-import axios from "axios";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
 
 // import data from "../data";
-import { useEffect,useReducer, useState} from "react";
+import { useEffect, useReducer } from 'react';
 
-const reducer=(state, action)=>{
-  switch (action.type){
-    case "FETCH_REQUEST":
-      return{...state, loading: true};
-    case "FETCH_success":
-      return{...state, loading: false, products: action.payload};
-    case "FETCH_ERROR":
-      return{...state, loading: false, error: action.payload};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_ERROR':
+      return { ...state, loading: false, error: action.payload };
     default:
       return state;
-
-
-
   }
-
-}
-
+};
 
 const HomeScreen = () => {
   // const [products, setProducts] = useState([])
-  const [{loading, error, products}, dispatch] = useReducer(reducer,{
-    products:[],
-    error:'',
-    loading:true
-    
-    
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
 
+    error: '',
+    products: [],
   });
 
- 
- useEffect(()=>{
-  const fetchData =async ()=>{
-    dispatch({type: 'FETCH_REQUEST'});
-    try{
-      const result = await axios.get('/api/products')
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        // setProducts(result.data)
+
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_ERROR ', payload: err.message });
+      }
+
       // setProducts(result.data)
-      
-      dispatch({type: 'FETCH_SUCCESS',payload : result.data})
+    };
+    fetchData();
+  }, []);
 
-
-    }
-    catch(err){
-      dispatch({type: 'FETCH_ERROR',payload : err.message});
-    }
-
-  
-    // setProducts(result.data)
-
-  }
-  fetchData()
-
-  },[])
-
-
-
-  return <div> <h1>Product features</h1>
-  <div className="products">
-    {products.map((product) => (
-      <div className="product" key={product.slug}>
-        <Link to={`/product/${product.slug}`}>
-        <img src={product.image} alt={product.name} />
-        </Link >
-        <div className='product-info'>
-        <Link  to={`/product/${product.slug}`}>
-          <p> {product.name}</p>
-          </Link >
-          <p><strong>${product.price}
-          </strong></p>
-          <button>Add cart</button>
-        </div>
+  return (
+    <div>
+      {' '}
+      <h1>Product features</h1>
+      <div className="products">
+        {loading ? (
+          <div>loading....</div>
+        ) : error ? (
+          <div>error....{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.slug}>
+              <Link to={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </Link>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p> {product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add cart</button>
+              </div>
+            </div>
+          ))
+        )}
       </div>
-    ))}
-  </div></div>;
+    </div>
+  );
 };
 
 export default HomeScreen;
